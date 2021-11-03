dede160814bf350fd3ebfc8c7c411633ecd2e5de
Vinicius Moura <viniciusrebelo487@gmail.com>
2021-11-03 20:46:35 -0300

    App one started

---
+++ app-one/src/App.js
@@ -0,0 +1,16 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <>
+      <h1>App one</h1>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+}
+
+export default App;
